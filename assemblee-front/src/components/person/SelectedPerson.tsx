ee153144595b1e0a7eb7107806a1551524409db7
Vianney FAIVRE <vianney.faivre@gmail.com>
2018-12-17 20:14:29 +0100

    display the list of all other mandates

--- assemblee-front/src/components/person/SelectedPerson.tsx
+++ assemblee-front/src/components/person/SelectedPerson.tsx
@@ -13,10 +13,21 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
 
         const genderLabel = props.selectedPerson.gender === "FEMALE" ? 'Ma députée' : 'Mon député';
         const mainMandateEnded: boolean = props.mandates.mainMandate.endDate !== null;
+
+        const otherMandatesTableRows = props.mandates.otherMandates.map((mandate) => 
+            <tr key={mandate.mandateId}>
+                <td>{mandate.politicalBodyLabel}</td>
+                <td>{mandate.politicalBodyType}</td>
+                <td>
+                    {mandate.endDate && <span>du {mandate.startDate} au {mandate.endDate}</span>}
+                    {!mandate.endDate && <span>depuis le {mandate.startDate}</span>}
+                </td>
+            </tr>
+        );
         
         return (
             <div>
-                <div>{genderLabel} : {props.selectedPerson.lastName} {props.selectedPerson.firstName}</div>
+                <h2 className="title">{genderLabel} : {props.selectedPerson.lastName} {props.selectedPerson.firstName}</h2>
 
                 <div>
                     Mandat principal: {props.mandates.mainMandate.politicalBodyLabel}&nbsp;
@@ -28,6 +39,22 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
                     {!mainMandateEnded && 
                         <span>(depuis le {props.mandates.mainMandate.startDate})</span>
                     }
+
+                    <h3 className="title">Ses mandats : </h3>
+
+                    <table className="table">
+
+                        <thead>
+                            <th>Nom du mandat</th>
+                            <th>Type de mandat</th>
+                            <th>Période</th>
+                        </thead>
+
+                        <tbody>
+                            {otherMandatesTableRows}
+                        </tbody>
+
+                    </table>
                 </div>
             </div>
         );
