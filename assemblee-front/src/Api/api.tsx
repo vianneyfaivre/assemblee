5552eb6fd2ebb2a0cd6e7240e615ab951f021133
Vianney FAIVRE <vianney.faivre@gmail.com>
2018-12-17 21:47:23 +0100

    convert ISO String to Dates via an Axios Transformer

--- assemblee-front/src/Api/api.tsx
+++ assemblee-front/src/Api/api.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosPromise } from 'axios';
+import axios, { AxiosPromise, AxiosTransformer } from 'axios';
 import PersonSearchItem from 'src/model/PersonSearchItem';
 import PersonMandates from 'src/model/PersonMandates';
 
@@ -22,10 +22,31 @@ export default class Api {
      * Returns a promise that contains the mandates a person is assigned to
      */
     public static getPersonMandates(personId: string): AxiosPromise<PersonMandates> {
+
+        let responseTransformers: AxiosTransformer[] = [];
+        if(axios.defaults.transformResponse){
+            responseTransformers = responseTransformers.concat(axios.defaults.transformResponse);
+        }
+
+        // Convert ISO dates (string) to Javascript Date
+        responseTransformers.push((data, headers) => {
+
+            data.mainMandate.startDate = new Date(Date.parse(data.mainMandate.startDate));
+            data.mainMandate.endDate = new Date(Date.parse(data.mainMandate.endDate));
+
+            data.otherMandates.forEach((mandate: any) => {
+                mandate.startDate = new Date(Date.parse(mandate.startDate));
+                mandate.endDate = new Date(Date.parse(mandate.endDate));
+            });
+
+            return data;
+        });
+
         return axios.request<PersonMandates>(
             {
                 method: 'get',
                 url: process.env.REACT_APP_ASSEMBLEE_BACKEND_URL + '/persons/'+personId+'/mandates',
+                transformResponse:  responseTransformers
             });
     }
 
--- assemblee-front/src/components/person/SelectedPerson.tsx
+++ assemblee-front/src/components/person/SelectedPerson.tsx
@@ -14,15 +14,19 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
         const genderLabel = props.selectedPerson.gender === "FEMALE" ? 'Ma députée' : 'Mon député';
         const mainMandateEnded: boolean = props.mandates.mainMandate.endDate !== null;
 
-        const otherMandatesTableRows = props.mandates.otherMandates.map((mandate) => 
-            <tr key={mandate.mandateId}>
-                <td>{mandate.politicalBodyLabel}</td>
-                <td>{mandate.politicalBodyType}</td>
-                <td>
-                    {mandate.endDate && <span>du {mandate.startDate} au {mandate.endDate}</span>}
-                    {!mandate.endDate && <span>depuis le {mandate.startDate}</span>}
-                </td>
-            </tr>
+        const otherMandatesTableRows = props.mandates.otherMandates.map((mandate) => {
+
+            return (
+                <tr key={mandate.mandateId}>
+                    <td>{mandate.politicalBodyLabel}</td>
+                    <td>{mandate.politicalBodyType}</td>
+                    <td>
+                        {mandate.endDate && <span>du {mandate.startDate.toLocaleDateString('fr-FR')} au {mandate.endDate.toLocaleDateString('fr-FR')}</span>}
+                        {!mandate.endDate && <span>depuis le {mandate.startDate.toLocaleDateString('fr-FR')}</span>}
+                    </td>
+                </tr>
+            )
+        }
         );
         
         return (
@@ -30,14 +34,14 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
                 <h2 className="title">{genderLabel} : {props.selectedPerson.lastName} {props.selectedPerson.firstName}</h2>
 
                 <div>
-                    Mandat principal: {props.mandates.mainMandate.politicalBodyLabel}&nbsp;
+                    Mandat principal à l'{props.mandates.mainMandate.politicalBodyLabel}&nbsp;
 
                     {mainMandateEnded && 
-                        <span>(du {props.mandates.mainMandate.startDate} au {props.mandates.mainMandate.endDate})</span>
+                        <span>(du {props.mandates.mainMandate.startDate.toLocaleDateString('fr-FR')} au {props.mandates.mainMandate.endDate!.toLocaleDateString('fr-FR')})</span>
                     }
 
                     {!mainMandateEnded && 
-                        <span>(depuis le {props.mandates.mainMandate.startDate})</span>
+                        <span>(depuis le {props.mandates.mainMandate.startDate.toLocaleDateString('fr-FR')})</span>
                     }
 
                     <h3 className="title">Ses mandats : </h3>
@@ -45,9 +49,11 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
                     <table className="table">
 
                         <thead>
-                            <th>Nom du mandat</th>
-                            <th>Type de mandat</th>
-                            <th>Période</th>
+                            <tr>
+                                <th>Nom du mandat</th>
+                                <th>Type de mandat</th>
+                                <th>Période</th>
+                            </tr>
                         </thead>
 
                         <tbody>
