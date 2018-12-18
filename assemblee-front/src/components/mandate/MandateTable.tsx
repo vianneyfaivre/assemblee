f7253916b1116366f5aba65f9cf687ae2ced1053
Vianney FAIVRE <vianney.faivre@gmail.com>
2018-12-18 09:36:44 +0100

    created new component that displays a list of mandates

---
+++ assemblee-front/src/components/mandate/MandateTable.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Mandate from 'src/model/Mandate';
+
+interface IMandateTableProps {
+    title: string,
+    mandates?: Mandate[],
+}
+
+export const MandateTable : React.StatelessComponent<IMandateTableProps> = (props) => {
+    
+    if(!props.mandates) {
+        return <div />;
+    }
+
+    const rows = props.mandates.map((mandate) => {
+        return (
+            <tr key={mandate.mandateId}>
+                <td>{mandate.politicalBodyLabel}</td>
+                <td>{mandate.politicalBodyType}</td>
+                <td>
+                    {mandate.endDate && <span>du {mandate.startDate.toLocaleDateString('fr-FR')} au {mandate.endDate.toLocaleDateString('fr-FR')}</span>}
+                    {!mandate.endDate && <span>depuis le {mandate.startDate.toLocaleDateString('fr-FR')}</span>}
+                </td>
+            </tr>
+        )
+     }
+    );
+
+    return (
+        <div>
+            <h3 className="title">{props.title} : </h3>
+
+            <table className="table">
+
+                <thead>
+                    <tr>
+                        <th>Nom du mandat</th>
+                        <th>Type de mandat</th>
+                        <th>Période</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {rows}
+                </tbody>
+
+            </table>
+        </div>
+    );
+}
--- assemblee-front/src/components/person/SelectedPerson.tsx
+++ assemblee-front/src/components/person/SelectedPerson.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import PersonSearchItem from 'src/model/PersonSearchItem';
 import PersonMandates from 'src/model/PersonMandates';
+import { MandateTable } from '../mandate/MandateTable';
 
 interface ISelectedPersonProps {
     selectedPerson?: PersonSearchItem,
@@ -14,21 +15,6 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
         const genderLabel = props.selectedPerson.gender === "FEMALE" ? 'Ma députée' : 'Mon député';
         const mainMandateEnded: boolean = props.mandates.mainMandate.endDate !== null;
 
-        const otherMandatesTableRows = props.mandates.otherMandates.map((mandate) => {
-
-            return (
-                <tr key={mandate.mandateId}>
-                    <td>{mandate.politicalBodyLabel}</td>
-                    <td>{mandate.politicalBodyType}</td>
-                    <td>
-                        {mandate.endDate && <span>du {mandate.startDate.toLocaleDateString('fr-FR')} au {mandate.endDate.toLocaleDateString('fr-FR')}</span>}
-                        {!mandate.endDate && <span>depuis le {mandate.startDate.toLocaleDateString('fr-FR')}</span>}
-                    </td>
-                </tr>
-            )
-        }
-        );
-        
         return (
             <div>
                 <h2 className="title">{genderLabel} : {props.selectedPerson.lastName} {props.selectedPerson.firstName}</h2>
@@ -44,23 +30,8 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
                         <span>(depuis le {props.mandates.mainMandate.startDate.toLocaleDateString('fr-FR')})</span>
                     }
 
-                    <h3 className="title">Ses mandats : </h3>
+                    <MandateTable title='Ses Mandats' mandates={props.mandates.otherMandates} />
 
-                    <table className="table">
-
-                        <thead>
-                            <tr>
-                                <th>Nom du mandat</th>
-                                <th>Type de mandat</th>
-                                <th>Période</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {otherMandatesTableRows}
-                        </tbody>
-
-                    </table>
                 </div>
             </div>
         );
