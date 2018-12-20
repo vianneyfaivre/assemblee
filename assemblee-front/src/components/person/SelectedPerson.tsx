87bc444a31f9b568e72f405a2e50aa987c7cf00f
Vianney FAIVRE <vianney.faivre@gmail.com>
2018-12-20 10:01:53 +0100

    added profile pic

--- assemblee-front/src/components/person/SelectedPerson.tsx
+++ assemblee-front/src/components/person/SelectedPerson.tsx
@@ -3,6 +3,7 @@ import PersonSearchItem from 'src/model/PersonSearchItem';
 import PersonMandates from 'src/model/PersonMandates';
 import { MandateTable } from 'src/components/mandate/MandateTable';
 import { MainMandate } from 'src/components/mandate/MainMandate';
+import Mandate from 'src/model/Mandate';
 
 interface ISelectedPersonProps {
     selectedPerson: PersonSearchItem,
@@ -13,12 +14,18 @@ export const SelectedPerson: React.StatelessComponent<ISelectedPersonProps> = (p
 
     const genderLabel = props.selectedPerson.gender === "FEMALE" ? 'Ma députée' : 'Mon député';
 
+    const getProfilePic = (mainMandate: Mandate, selectedPerson: PersonSearchItem) => {
+       return `http://www2.assemblee-nationale.fr/static/tribun/${mainMandate.legislature}/photos/${selectedPerson.id.split("PA")[1]}.jpg`;
+    };
+
     return (
         <div>
             <h2 className="title">{genderLabel} : {props.selectedPerson.lastName} {props.selectedPerson.firstName}</h2>
 
             {props.mandates &&
                 <div>
+                    <img src={getProfilePic(props.mandates.mainMandate, props.selectedPerson)} alt={props.selectedPerson.lastName + ' ' + props.selectedPerson.firstName} />
+    
                     <MainMandate mandate={props.mandates.mainMandate} />
 
                     <MandateTable title='Ses participations au gouvernement' mandatesGrouped={props.mandates.governmentMandates} />
