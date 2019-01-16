8449c362e76ae2f86fd5f7f71e7d76223ff3e347
Vianney FAIVRE <vianney.faivre@gmail.com>
2019-01-16 17:14:29 +0100

    removed unused classes

--- assemblee-rest/src/main/java/re/vianneyfaiv/assemblee/dao/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package re.vianneyfaiv.assemblee.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import re.vianneyfaiv.assemblee.model.jpa.Vote;
-
-public interface VoteRepository extends JpaRepository<Vote, String> {
-}
